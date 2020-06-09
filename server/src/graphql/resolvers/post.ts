aa1a90dacd77c3a28a2009cf28428b90a7d21288
xeusteerapat <xeus085@gmail.com>
2020-06-09 19:01:26 +0700

    refactor resolvers function for relational data

--- server/src/graphql/resolvers/post.ts
+++ server/src/graphql/resolvers/post.ts
@@ -1,18 +1,10 @@
-import { getRepository } from 'typeorm';
-import { User } from './../../entity/User';
-import { Comment } from './../../entity/Comment';
-
 const Post = {
-  // author: async (parent, args, context, info) => {
-  //   const userRepository = await getRepository(User);
-  //   const authors = await userRepository.find({
-  //     relations: ['posts', 'comments'],
-  //   });
-  //   const authorToPost = authors.filter(
-  //     author => author.id === parent.author.id
-  //   );
-  //   return authorToPost;
-  // },
+  author: async (parent, args, context, info) => {
+    return parent.author;
+  },
+  comments: async (parent, args, context, info) => {
+    return parent.comments;
+  },
 };
 
 export default Post;
