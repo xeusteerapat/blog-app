0450e9c003380e7a075759f4d99ca57ce86078e2
xeusteerapat <xeus085@gmail.com>
2020-06-14 00:09:36 +0700

    connect apollo client

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
-function App() {
+interface Post {
+  id: string;
+  title: string;
+  body: string;
+}
+
+interface User {
+  id: string;
+  username: string;
+  posts: Post[];
+}
+
+const GET_USERS = gql`
+  query {
+    users {
+      id
+      username
+      posts {
+        id
+        title
+        body
+      }
+    }
+  }
+`;
+
+const App = () => {
+  const { loading, data } = useQuery(GET_USERS);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  const { users } = data;
   return (
     <div className='App'>
-      <h1>Hi There</h1>
+      <ul>
+        {users.map((user: User) => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:5500',
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
