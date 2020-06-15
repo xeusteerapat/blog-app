084f11c755ef95ef2caf4f31101f6cbcb473ff62
xeusteerapat <xeus085@gmail.com>
2020-06-15 16:28:27 +0700

    add useForm

--- client/src/components/pages/Register.tsx
+++ client/src/components/pages/Register.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import {
   StyledFormWrapper,
   StyledForm,
@@ -6,42 +7,40 @@ import {
   StyledButton,
 } from '../styles/styles';
 
+type FormData = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const Register = () => {
-  const handleInput = (e: React.FormEvent) => {};
-  const handleSubmit = (e: React.FormEvent) => {};
+  const { register, setValue, handleSubmit, errors } = useForm<FormData>();
+  const onSubmit = handleSubmit(
+    ({ username, email, password, confirmPassword }) => {
+      console.log({ username, email, password, confirmPassword });
+      setValue('username', '');
+      setValue('email', '');
+      setValue('password', '');
+      setValue('confirmPassword', '');
+    }
+  );
+
   return (
     <StyledFormWrapper>
-      <StyledForm onSubmit={handleSubmit}>
+      <StyledForm onSubmit={onSubmit}>
         <h2>Register</h2>
         <label htmlFor='username'>Username</label>
-        <StyledInput
-          type='text'
-          name='username'
-          // value={state.name}
-          onChange={handleInput}
-        />
+        <StyledInput type='text' name='username' ref={register} />
         <label htmlFor='email'>Email</label>
-        <StyledInput
-          type='email'
-          name='email'
-          // value={state.email}
-          onChange={handleInput}
-        />
+        <StyledInput type='email' name='email' ref={register} />
         <label htmlFor='password'>Password</label>
-        <StyledInput
-          type='password'
-          name='password'
-          // value={state.email}
-          onChange={handleInput}
-        />
-        <label htmlFor='confirmpassword'>Confirm Password</label>
-        <StyledInput
-          type='password'
-          name='confirmpassword'
-          // value={state.email}
-          onChange={handleInput}
-        />
-        <StyledButton type='submit'>Register</StyledButton>
+        <StyledInput type='password' name='password' ref={register} />
+        <label htmlFor='confirmPassword'>Confirm Password</label>
+        <StyledInput type='password' name='confirmPassword' ref={register} />
+        <StyledButton type='submit' onClick={() => {}}>
+          Register
+        </StyledButton>
       </StyledForm>
     </StyledFormWrapper>
   );
