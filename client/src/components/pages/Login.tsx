1bf771e7766381a1f5685d8995fbaf4f0726eb4e
xeusteerapat <xeus085@gmail.com>
2020-06-17 22:59:13 +0700

    finish login  and redirect

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useMutation } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import {
   StyledFormWrapper,
   StyledForm,
@@ -7,56 +9,90 @@ import {
   StyledButton,
   StyledInputWrapper,
   ValidateError,
+  ErrorBox,
 } from '../styles/styles';
+import Spinner from '../misc/Spinner';
 
 type FormData = {
   username: string;
   password: string;
 };
 
-const Login = () => {
-  const { register, setValue, handleSubmit, errors } = useForm<FormData>();
+const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      username
+      token
+    }
+  }
+`;
+
+const Login = (props: any) => {
+  const { register: loginUser, setValue, handleSubmit, errors } = useForm<
+    FormData
+  >();
+
+  const [login, { loading, error }] = useMutation(LOGIN, {
+    update: (_, result) => {
+      props.history.push('/');
+    },
+    onError: () => null,
+  });
+
   const onSubmit = handleSubmit(({ username, password }) => {
+    login({ variables: { username, password } });
     setValue('username', '');
     setValue('password', '');
   });
 
+  if (loading) return <Spinner />;
+
   return (
-    <StyledFormWrapper>
-      <StyledForm onSubmit={onSubmit}>
-        <h2>Login</h2>
-        <StyledInputWrapper>
-          <label htmlFor='username'>Username</label>
-          <StyledInput
-            type='text'
-            name='username'
-            ref={register({
-              required: true,
-            })}
-          />
-          <ValidateError>
-            {errors.username?.type === 'required' && 'Username is required'}
-          </ValidateError>
-        </StyledInputWrapper>
-        <StyledInputWrapper>
-          <label htmlFor='password'>Password</label>
-          <StyledInput
-            type='password'
-            name='password'
-            ref={register({
-              required: true,
-              minLength: 8,
-            })}
-          />
-          <ValidateError>
-            {errors.password?.type === 'required' && 'Password is required'}
-            {errors.password?.type === 'minLength' &&
-              'Password must be greater than 8 characters'}
-          </ValidateError>
-        </StyledInputWrapper>
-        <StyledButton type='submit'>Log In</StyledButton>
-      </StyledForm>
-    </StyledFormWrapper>
+    <>
+      {error && (
+        <>
+          {error.graphQLErrors.map(({ message }, i) => (
+            <ErrorBox key={i}>{message}</ErrorBox>
+          ))}
+        </>
+      )}
+      <StyledFormWrapper>
+        <StyledForm onSubmit={onSubmit}>
+          <h2>Login</h2>
+          <StyledInputWrapper>
+            <label htmlFor='username'>Username</label>
+            <StyledInput
+              type='text'
+              name='username'
+              ref={loginUser({
+                required: true,
+              })}
+            />
+            <ValidateError>
+              {errors.username?.type === 'required' && 'Username is required'}
+            </ValidateError>
+          </StyledInputWrapper>
+          <StyledInputWrapper>
+            <label htmlFor='password'>Password</label>
+            <StyledInput
+              type='password'
+              name='password'
+              ref={loginUser({
+                required: true,
+                minLength: 8,
+              })}
+            />
+            <ValidateError>
+              {errors.password?.type === 'required' && 'Password is required'}
+              {errors.password?.type === 'minLength' &&
+                'Password must be greater than 8 characters'}
+            </ValidateError>
+          </StyledInputWrapper>
+          <StyledButton type='submit'>Log In</StyledButton>
+        </StyledForm>
+      </StyledFormWrapper>
+    </>
   );
 };
 
