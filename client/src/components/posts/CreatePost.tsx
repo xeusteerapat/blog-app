faccf55d526d3bbed509687e02a36183feff0d73
xeusteerapat <xeus085@gmail.com>
2020-06-19 13:55:34 +0700

    add CKEditor component

--- client/src/components/posts/CreatePost.tsx
+++ client/src/components/posts/CreatePost.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+import {
+  StyledFormWrapper,
+  StyledForm,
+  StyledInput,
+  StyledButton,
+  StyledInputWrapper,
+  ValidateError,
+  ErrorBox,
+} from '../styles/styles';
 
 const CreatePost = () => {
   const accessToken = localStorage.getItem('accessToken');
+
+  const [value, setValue] = useState('');
+
+  const handleChange = (e: React.FormEventHandler, editor: any) => {
+    console.log(editor.getData());
+  };
 
   if (!accessToken) {
     return (
@@ -12,9 +30,21 @@ const CreatePost = () => {
   }
 
   return (
-    <div>
+    <>
       <h1>Create your post</h1>
-    </div>
+      <StyledFormWrapper>
+        <StyledForm>
+          <StyledInputWrapper>
+            <label htmlFor='title'>Title</label>
+            <StyledInput type='text' name='title' />
+          </StyledInputWrapper>
+          <StyledInputWrapper>
+            <CKEditor editor={ClassicEditor} onChange={handleChange} />
+          </StyledInputWrapper>
+          <StyledButton type='submit'>Create Post</StyledButton>
+        </StyledForm>
+      </StyledFormWrapper>
+    </>
   );
 };
 
--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module '@ckeditor/ckeditor5-react' {
+  const CKEditor: any;
+  export default CKEditor;
+}
+
+declare module '@ckeditor/ckeditor5-build-classic' {
+  const ClassicEditor: any;
+  export = ClassicEditor;
+}
