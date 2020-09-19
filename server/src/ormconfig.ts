6968dd132faa9b51b2969ec555654e74273525a9
xeusteerapat <xeus085@gmail.com>
2020-09-19 13:11:35 +0700

    [change] ormconfig

---
+++ server/src/ormconfig.ts
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export = {
+  host: process.env.TYPEORM_HOST,
+  type: process.env.TYPEORM_CONNECTION,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: [process.env.TYPEORM_ENTITIES],
+  synchronize: process.env.TYPEORM_SYNCHRONIZE,
+  logging: process.env.TYPEORM_LOGGING,
+} as const;
